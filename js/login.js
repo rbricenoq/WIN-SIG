99e5dcc6f9c4346ce5176251836faa2bedbd1057
Serbarfo182 <sergiobarfo19@gmail.com>
2017-03-24 22:36:05 -0500

    Version 1.0
    Mejoras Login

--- js/login.js
+++ js/login.js
@@ -6,7 +6,7 @@ particlesJS('particles-js',
         "value": 400,
         "density": {
           "enable": true,
-          "value_area": 800
+          "value_area": 900
         }
       },
       "color": {
