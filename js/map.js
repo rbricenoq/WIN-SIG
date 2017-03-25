97926b649c084d37b4fa23e5a01bb534e31b6e10
rbricenoq <rbricenoq@unbosque.edu.co>
2017-03-25 02:08:46 -0500

    Corrección pagina principal
    Arreglo de errores en la pagina principal

--- js/map.js
+++ js/map.js
@@ -1,7 +1,13 @@
 var map;
+var marker;
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 11.501007, lng: -72.549250},
-		zoom: 8
+		center: {lat: 11.910625, lng: -72.009213},
+		zoom: 10		
+	});
+	marker = new google.maps.Marker({
+		position: {lat: 11.801717, lng: -71.966775},
+		title:"Hello World!"
 	});
 }
+
